94a0c6bdc3a1d74f58bd327dc440a2065cfa1b5e
Raphaël Désalbres <raphael.desalbres@outlook.com>
2020-05-27 14:42:01 +0200

    Ajoutez des fichiers projet.

---
+++ CustomAuth/Credentials.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomAuth
+{
+    public class Credentials : IValidatableObject
+    {
+        
+        [Required]
+        public string login { get; set; }
+        [Required]
+        public string password { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            //In a real-world app there would be a database validation...
+            if(login!="john@doe.com" || password!="myp@ssword")
+            {
+                yield return new ValidationResult(
+                $"Wrong password or login !",
+                new[] { nameof(login), nameof(password)});
+            }
+        }
+    }
+}
---
+++ CustomAuth/CustomAuthStateProvider.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace CustomAuth
+{
+    public class MyAuthenticationStateProvider : AuthenticationStateProvider
+    {        
+        private bool IsAuthenticated { get; set; }
+        private string userName { get; set; }
+        public void setAuthentication(string _userName)
+        {
+            IsAuthenticated = !string.IsNullOrEmpty(_userName);
+            userName = _userName;                 
+            NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+        }
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            ClaimsIdentity identity;
+
+            if (IsAuthenticated)
+            {
+                identity = new ClaimsIdentity(new List<Claim>
+                        {
+                            new Claim(ClaimTypes.Name,userName)
+
+                        }, "CustomAuth");
+            }
+            else
+            {
+                identity = new ClaimsIdentity();
+            }           
+            return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(identity)));
+        }
+        
+    }
+}
+
